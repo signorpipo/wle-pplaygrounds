ce9faf7be3c0baec87b677667c6a9f81c9bc3168
Pipo <eliaducceschi@hotmail.it>
2023-03-14 12:51:24 +0100

    Add sfx on collision

--- wle_pplaygrounds/wle_pplayground/wle_pplayground_npm_unbundled/js/playground/bundle.js
+++ wle_pplaygrounds/wle_pplayground/wle_pplayground_npm_unbundled/js/playground/bundle.js
@@ -1,2 +1,4 @@
+require("./playground_gateway");
 require("./grabbable_spawner");
 require("./scale_on_spawn");
+require("./sfx_on_collision");
---
+++ wle_pplaygrounds/wle_pplayground/wle_pplayground_npm_unbundled/js/playground/playground_gateway.js
@@ -0,0 +1,54 @@
+WL.registerComponent("playground-gateway", {
+}, {
+    init() {
+        this._loadAudio();
+    },
+    start() {
+    },
+    update(dt) {
+
+    },
+    _loadAudio() {
+        let manager = PP.myAudioManager;
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/music/pp/playground_ambient.mp3");
+            audioSetup.myLoop = true;
+            audioSetup.mySpatial = false;
+            audioSetup.myVolume = 1;
+            manager.addAudioSetup("playground_ambient", audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/pp/collision.mp3");
+            audioSetup.myRate = 1;
+            audioSetup.myVolume = 1;
+            audioSetup.myReferenceDistance = 5;
+            manager.addAudioSetup("collision", audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/pp/grab.mp3");
+            audioSetup.myRate = 1;
+            audioSetup.myVolume = 1;
+            audioSetup.myReferenceDistance = 0.3;
+            manager.addAudioSetup("grab", audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/pp/throw.mp3");
+            audioSetup.myRate = 1;
+            audioSetup.myVolume = 1;
+            audioSetup.myReferenceDistance = 0.3;
+            manager.addAudioSetup("throw", audioSetup);
+        }
+
+        {
+            let audioSetup = new PP.AudioSetup("assets/audio/sfx/pp/strike.mp3");
+            audioSetup.myRate = 1;
+            audioSetup.myVolume = 1;
+            audioSetup.myReferenceDistance = 3;
+            manager.addAudioSetup("strike", audioSetup);
+        }
+    }
+});
