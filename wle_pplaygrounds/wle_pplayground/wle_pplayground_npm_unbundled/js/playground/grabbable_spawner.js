497a0524af9e111e423a55558778b7e4e1e03388
Pipo <eliaducceschi@hotmail.it>
2023-03-14 18:00:03 +0100

    Add spawner delay

--- wle_pplaygrounds/wle_pplayground/wle_pplayground_npm_unbundled/js/playground/grabbable_spawner.js
+++ wle_pplaygrounds/wle_pplayground/wle_pplayground_npm_unbundled/js/playground/grabbable_spawner.js
@@ -5,25 +5,30 @@ WL.registerComponent("grabbable-spawner", {
     },
     start() {
         this._myPrototypes = this._myPrototypesContainer.pp_getChildren();
-
         this._myCurrentGrabbable = null;
 
-        this._myStarted = false;
+        this._myFirstUpdate = true;
+        this._myStartTimer = new PP.Timer(0.5);
     },
     update(dt) {
-        if (!this._myStarted) {
-            this._myStarted = true;
+        if (this._myFirstUpdate) {
+            this._myFirstUpdate = false;
 
             for (let prototype of this._myPrototypes) {
                 prototype.pp_setActive(false);
             }
-
-            this._spawn();
-        }
-
-        if (this._myCurrentGrabbable != null) {
-            if (this.object.pp_getPosition().vec3_distance(this._myCurrentGrabbable.pp_getPosition()) > 0.2) {
-                this._spawn();
+        } else {
+            if (this._myStartTimer.isRunning()) {
+                this._myStartTimer.update(dt);
+                if (this._myStartTimer.isDone()) {
+                    this._spawn();
+                }
+            } else {
+                if (this._myCurrentGrabbable != null) {
+                    if (this.object.pp_getPosition().vec3_distance(this._myCurrentGrabbable.pp_getPosition()) > 0.2) {
+                        this._spawn();
+                    }
+                }
             }
         }
     },
