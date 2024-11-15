dd5a276713503097a37715db2f78c66be46c0158
Pipo <eliaducceschi@hotmail.it>
2024-11-15 17:47:50 +0100

    Sync PP

--- wle_pplaygrounds/wle_pplayground/wle_pplayground_pipo/src/playground/components/particles_spawner_component.js
+++ wle_pplaygrounds/wle_pplayground/wle_pplayground_pipo/src/playground/components/particles_spawner_component.js
@@ -10,7 +10,7 @@ export class ParticlesSpawnerComponent extends Component {
     };
 
     start() {
-        this._myFirstUpdate = true;
+        this._myStartFrameCountdown = 1;
     }
 
     _start() {
@@ -39,22 +39,26 @@ export class ParticlesSpawnerComponent extends Component {
     }
 
     update(dt) {
-        if (this._myFirstUpdate) {
-            this._start();
-            this._myFirstUpdate = false;
+        if (this._myStartFrameCountdown > 0) {
+            this._myStartFrameCountdown--;
+            if (this._myStartFrameCountdown == 0) {
+                this._start();
+            }
         }
     }
 
     spawn(position) {
-        let amount = Math.pp_randomInt(15, 30);
+        if (this._myStartFrameCountdown == 0) {
+            let amount = Math.pp_randomInt(15, 30);
 
-        for (let i = 0; i < amount; i++) {
-            let particle = Globals.getObjectPoolManager(this.engine).get(this._getParticlePoolID(Math.pp_randomInt(0, this._myParticles.length - 1)));
-            particle.pp_getComponent(ParticleComponent).onDone(this._onParticleDone.bind(this, particle));
+            for (let i = 0; i < amount; i++) {
+                let particle = Globals.getObjectPoolManager(this.engine).get(this._getParticlePoolID(Math.pp_randomInt(0, this._myParticles.length - 1)));
+                particle.pp_getComponent(ParticleComponent).onDone(this._onParticleDone.bind(this, particle));
 
-            particle.pp_setPosition(position.vec3_add(particle.pp_getComponent(ParticleComponent)._myHorizontalSpeed.vec3_normalize().vec3_scale(Math.pp_random(0, this._myRadius))));
+                particle.pp_setPosition(position.vec3_add(particle.pp_getComponent(ParticleComponent)._myHorizontalSpeed.vec3_normalize().vec3_scale(Math.pp_random(0, this._myRadius))));
 
-            particle.pp_setActive(true);
+                particle.pp_setActive(true);
+            }
         }
     }
 
